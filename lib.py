c9ab6d8421ac14dc00c9fb041a7feada3c2e2aa9
Marika Kapanadze <marika.kapanadze.mit08@adontz.com>
2023-06-25 12:08:20 +0000

    executing pytest-passed-black-failed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 9405168769489823392
+# 1392116876949001329
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,4 @@
-# 1343416876948983759
+# 29990168769490016523
 from lib import QuadraticEquationSolver
 from pytest import approx, raises
 
