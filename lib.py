4c459ce56136e6c8436e3b86b7f8ad8aa84399d1
Levan Chkhaidze <levan.chkhaidze.mit08@adontz.com>
2023-06-25 12:00:57 +0000

    executing pytest-failed-black-failed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 1971416876944259178
+# 18117168769445715024
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,4 @@
-# 957816876944255650
+# 7148168769445729931
 from lib import QuadraticEquationSolver
 from pytest import approx, raises
 
