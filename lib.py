a3a0114e8f81de8f77af50dd1167868ef81489df
Levan Chkhaidze <levan.chkhaidze.mit08@adontz.com>
2023-06-25 21:02:55 +0000

    executing pytest-syntax-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 19504168772691412952
-class QuadraticEquationSolver:
+# 6082168772697510593
+class QuadraticEquationSolver
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
