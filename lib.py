649270ea5f5ce9bac4085bcd95ff13a136bccedf
Zurab Kapanadze <zurab.kapanadze.mit08@adontz.com>
2023-06-27 05:50:59 +0000

    executing pytest-absent-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 2577416878449989741
+# 2654616878450594613
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d <= 0:
+        if d < 0:
             return []
 
         if d == 0:
