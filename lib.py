5d062c2b7023e9b6821337d69e37f944e531e75e
Sandro Imerlishvili <sandro.imerlishvili.mit08@adontz.com>
2023-06-27 20:20:18 +0000

    executing pytest-failed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 287316878971578655
+# 837816878972183319
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
