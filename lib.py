a1e7254c4cca3c88a31abd7bbc7cc6ced60b7962
Nino Chkheidze <nino.chkheidze.mit08@adontz.com>
2023-06-27 22:37:23 +0000

    executing pytest-syntax-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 14464168790538213132
-class QuadraticEquationSolver:
+# 133516879054429356
+class QuadraticEquationSolver
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b ** 2 - 4 * self.a * self.c
 
-        if d <= 0: return []
+        if d < 0: return []
 
         if d == 0:
             return [-self.b / (2 * self.a)]
--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,4 @@
-# 31052168790538223029
+# 27261168790544225527
 from lib import QuadraticEquationSolver
 from pytest import approx, raises
 
