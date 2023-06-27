544ae3e83be07b895035508c3fd3fba44354b62d
Irakli Gogaladze <irakli.gogaladze.mit08@adontz.com>
2023-06-27 17:54:04 +0000

    executing pytest-absent-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 7255168788838429953
-class QuadraticEquationSolver
+# 20140168788844425569
+class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
