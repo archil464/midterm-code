71c49b94ffc36eae7074449ee06065609b91c576
Levan Mikeladze <levan.mikeladze.mit08@adontz.com>
2023-06-28 23:34:22 +0000

    executing pytest-absent-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 17408168799520231646
-class QuadraticEquationSolver
+# 32013168799526221402
+class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
