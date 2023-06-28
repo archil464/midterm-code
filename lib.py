20d71c4cbf35a43524830a676226478f71e837fc
Giorgi Beridze <giorgi.beridze.mit08@adontz.com>
2023-06-28 22:27:21 +0000

    executing pytest-failed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 1174168799117924614
+# 25431168799124132091
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
