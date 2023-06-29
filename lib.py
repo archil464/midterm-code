283fced33596a0527b18962eb5b5e23e2a8d3c71
Nino Chkheidze <nino.chkheidze.mit08@adontz.com>
2023-06-29 13:15:58 +0000

    executing pytest-failed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 12152168804449729615
+# 889516880445583730
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
