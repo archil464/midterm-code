3568dbcd7c2a62cb79457c7c491814eb097ca26e
Davit Khizanishvili <davit.khizanishvili.mit08@adontz.com>
2023-06-29 20:55:27 +0000

    executing pytest-syntax-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 6785168807206618466
-class QuadraticEquationSolver:
+# 12534168807212720862
+class QuadraticEquationSolver
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
