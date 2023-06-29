9a7de663c076fc385613a2ad277299aa2a2450a8
Nino Maisuradze <nino.maisuradze.mit08@adontz.com>
2023-06-29 11:11:03 +0000

    executing pytest-absent-black-passed

--- lib.py
+++ lib.py
@@ -1,19 +1,20 @@
-# 17597168803700215249
-class QuadraticEquationSolver
+# 31790168803706315739
+class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
 
     def solve(self):
-        d = self.b ** 2 - 4 * self.a * self.c
+        d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0: return []
+        if d < 0:
+            return []
 
         if d == 0:
             return [-self.b / (2 * self.a)]
 
         return [
-            (-self.b + d ** 0.5) / (2 * self.a),
-            (-self.b - d ** 0.5) / (2 * self.a),
+            (-self.b + d**0.5) / (2 * self.a),
+            (-self.b - d**0.5) / (2 * self.a),
         ]
