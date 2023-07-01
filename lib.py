682ce247d82259c6f45aa86658750d12df0ea4ea
Salome Dadiani <salome.dadiani.mit08@adontz.com>
2023-07-01 19:31:20 +0000

    executing pytest-failed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 21683168823986314214
+# 28122168823988011816
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,4 @@
-# 15055168823986324540
+# 1174816882398808038
 from lib import QuadraticEquationSolver
 from pytest import approx, raises
 
