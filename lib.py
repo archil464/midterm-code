c4ec8ae35fbb875a1849c9a62bce5640983737f3
Nana Abashidze <nana.abashidze.mit08@adontz.com>
2023-07-01 14:42:34 +0000

    executing pytest-syntax-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 8535168822253816211
-class QuadraticEquationSolver:
+# 24305168822255422198
+class QuadraticEquationSolver
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
