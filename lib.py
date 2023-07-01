6dead47d21824b4d319b1b87f00426eadb4a68e8
Irakli Maisuradze <irakli.maisuradze.mit08@adontz.com>
2023-07-01 12:41:30 +0000

    executing pytest-failed-black-failed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 21288168821527518158
+# 31500168821529022478
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b ** 2 - 4 * self.a * self.c
 
-        if d < 0: return []
+        if d <= 0: return []
 
         if d == 0:
             return [-self.b / (2 * self.a)]
