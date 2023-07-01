5da4d979fdc90d2ba3dcc141130fa6326362c20d
Davit Khizanishvili <davit.khizanishvili.mit08@adontz.com>
2023-07-01 23:28:07 +0000

    executing pytest-absent-black-failed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 2376616882540714218
+# 22293168825408713653
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b ** 2 - 4 * self.a * self.c
 
-        if d <= 0: return []
+        if d < 0: return []
 
         if d == 0:
             return [-self.b / (2 * self.a)]
