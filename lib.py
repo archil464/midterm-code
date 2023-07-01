7d1a94a21e62e0a4c417f8979e607e5c52cb3e2a
Salome Dadiani <salome.dadiani.mit08@adontz.com>
2023-07-01 17:43:32 +0000

    executing pytest-failed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 626516882333972743
+# 1955316882334127429
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
