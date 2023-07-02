2fc0203d48e5b7483adeabb8535b70ccbe2cff5f
Davit Khizanishvili <davit.khizanishvili.mit08@adontz.com>
2023-07-02 17:17:09 +0000

    executing pytest-syntax-black-failed

--- lib.py
+++ lib.py
@@ -1,20 +1,19 @@
-# 32212168831821426221
-class QuadraticEquationSolver:
+# 17280168831822915315
+class QuadraticEquationSolver
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
 
     def solve(self):
-        d = self.b**2 - 4 * self.a * self.c
+        d = self.b ** 2 - 4 * self.a * self.c
 
-        if d < 0:
-            return []
+        if d < 0: return []
 
         if d == 0:
             return [-self.b / (2 * self.a)]
 
         return [
-            (-self.b + d**0.5) / (2 * self.a),
-            (-self.b - d**0.5) / (2 * self.a),
+            (-self.b + d ** 0.5) / (2 * self.a),
+            (-self.b - d ** 0.5) / (2 * self.a),
         ]
