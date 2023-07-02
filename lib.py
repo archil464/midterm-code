e939aa0e6fc63675ffb6d97036b79948d1a673e8
Maia Gogoladze <maia.gogoladze.mit08@adontz.com>
2023-07-02 11:35:09 +0000

    executing pytest-failed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 2608116882976948074
+# 18051688297709236
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
