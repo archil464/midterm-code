e4a2499c951b0e78be814e2971f9e9630d3f68f8
Sofia Mikeladze <sofia.mikeladze.mit08@adontz.com>
2023-07-02 12:31:27 +0000

    executing pytest-absent-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 15066168830107011229
+# 24915168830108716588
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d <= 0:
+        if d < 0:
             return []
 
         if d == 0:
