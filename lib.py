cc505dd09dc248c281726e156f927d27587b7b9d
Giorgi Nozadze <giorgi.nozadze.mit08@adontz.com>
2023-07-02 16:14:46 +0000

    executing pytest-syntax-black-failed

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
-# 14906168831447128021
-class QuadraticEquationSolver:
+# 2164616883144869516
+class QuadraticEquationSolver
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
