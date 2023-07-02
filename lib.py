bfa3a4fff7b8b2d7b1e883254cfe14f1ee426c71
Nana Abashidze <nana.abashidze.mit08@adontz.com>
2023-07-02 03:51:06 +0000

    executing pytest-passed-black-passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 12237168826985112184
+# 12085168826986615221
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,4 @@
-# 26636168826985122581
+# 26129168826986630399
 from lib import QuadraticEquationSolver
 from pytest import approx, raises
 
